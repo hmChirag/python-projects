cd6c639aaeca41b18c45ca5473a1caa766e30610
Chirag H M <chiraghm2004@gmail.com>
2024-04-08 12:21:15 +0530

    robo speaker

--- robospeaker.py
+++ robospeaker.py
@@ -3,4 +3,4 @@ import playsound
 text=input("enter the text you want to say")
 sound=gtts.gTTS(text,lang="en") 
 sound.save("welcome.mp3")
-playsound.playsound("welcome.mp3")
+playsound.playsound("welcome.mp3") 
